c4415f68dfff08af5f0f90f603848832b2b2e8d0
luiscamaral <3392839+luiscamaral@users.noreply.github.com>
2025-05-09 10:18:44 -0600

    Updated conda env file name to .conda-env

--- conda_auto_env.sh
+++ conda_auto_env.sh
@@ -1,7 +1,5 @@
-#!/bin/bash
-
 # conda-auto-env automatically activates a conda environment when
-# entering a folder with an environment.yml file.
+# entering a folder with an '.conda-env' file.
 #
 # If the environment doesn't exist, conda-auto-env creates it and
 # activates it for you.
@@ -10,25 +8,30 @@
 #
 #       source /path/to/conda_auto_env.sh
 #
+CONDA_AUTO_FILE=".conda-env"
+
 
 function conda_auto_env() {
-  if [ -e "environment.yml" ]; then
-    # echo "environment.yml file found"
-    ENV=$(head -n 1 environment.yml | cut -f2 -d ' ')
+  if [ -e "${CONDA_AUTO_FILE}" ]; then
+    echo "${CONDA_AUTO_FILE} file found"
+    ENV=$(head -n 1 ${CONDA_AUTO_FILE} | cut -f2 -d ' ')
     # Check if you are already in the environment
     if [[ $PATH != *$ENV* ]]; then
       # Check if the environment exists
-      source activate $ENV
+      conda activate $ENV
       if [ $? -eq 0 ]; then
         :
       else
-        # Create the environment and activate
-        echo "Conda env '$ENV' doesn't exist."
-        conda env create -q
-        source activate $ENV
+        echo "Conda environment name '${ENV}' doesn't exist."
       fi
     fi
   fi
 }
 
-export PROMPT_COMMAND=conda_auto_env
+# Detect the shell and set the hook accordingly
+if [[ $SHELL == *"zsh"* ]]; then
+  autoload -U add-zsh-hook
+  add-zsh-hook chpwd conda_auto_env
+elif [[ $SHELL == *"bash"* ]]; then
+  PROMPT_COMMAND="conda_auto_env; $PROMPT_COMMAND"
+fi
